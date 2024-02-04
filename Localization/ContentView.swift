d8a056d62789d714a1bebdec397f9d768f94c2c1
hossinasaadi <hossin.asaadi@gmail.com>
2024-02-04 17:45:39 +0400

    Initial Commit

---
+++ Localization/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Localization
+//
+//  Created by HossinAsaadi on 2/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Localization/LocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationApp.swift
+//  Localization
+//
+//  Created by HossinAsaadi on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
